2edc614628810eae16b3529f6accfd13dfeaa3be
Alfonsina <alfonsina.l@gmail.com>
2023-02-09 17:20:34 -0300

    feat(app): add apollo client integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,37 @@
 import PropTypes from 'prop-types';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
+import withData from '../lib/withData';
 import '../components/styles/nprogress.css';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-const MyApp = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
+const MyApp = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  </ApolloProvider>
 );
 
-export default MyApp;
+// If any of the pages have a getInitialProps method on them, (which they will because withData is adding it to them) then go fetch it
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
 
 MyApp.propTypes = {
   Component: PropTypes.any,
   pageProps: PropTypes.any,
+  apollo: PropTypes.any,
 };
